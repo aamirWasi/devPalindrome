31315fe6662e4430ce8912636f89b7a6778219e1
aamir <wasi3aamir@gmail.com>
2019-01-21 14:28:32 +0600

    Dev palindrome wrong answer

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int i = 0; i < T; i++)
+            {
+                string str = "";
+                string reverse = "";
+                int index = 0;
+                str = Console.ReadLine();
+                index = str.Length - 1;
+                while(index >= 0) {
+                    reverse += str.Substring(index, 1);
+                    index--;
+                }
+                if(str == reverse)
+                {
+                    Console.WriteLine("Yes");
+                }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            }
+        }
+    }
+}
